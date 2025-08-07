163fa60a1aaf206b295f7a0222723e333a2002fd
Pavel <savchukpa@icloud.com>
2025-08-07 18:20:39 +0300

    add: dataclass User

---
+++ test_data/__init__.py
@@ -0,0 +1,3 @@
+__all__ = ["User", "standard_user"]
+
+from .data_class_users.users import User, standard_user
---
+++ test_data/data_class_users/users.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from utils.data_generate_helper import DataGenerateHelper
+
+
+@dataclass
+class User(DataGenerateHelper):
+    username: str
+    password: str
+
+    def build_random_user(self):
+        self.username = self.faker.user_name()
+        self.password = self.faker.password()
+
+
+standard_user = User(username="standard_user", password="secret_sauce")
