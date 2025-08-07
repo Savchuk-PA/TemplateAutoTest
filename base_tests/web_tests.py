382f600a0c31ee451fed85a79cccd21050fdb7fc
Pavel <savchukpa@icloud.com>
2025-08-07 19:09:23 +0300

    add: test_login by standard user

--- base_tests/web_tests.py
+++ base_tests/web_tests.py
@@ -1,5 +1,6 @@
 import time
 
+import allure
 
 import web as web_pages
 import test_data
@@ -7,8 +8,12 @@ import test_data
 
 class TestUsers:
 
+    @allure.title("Test Users")
     def test_login(self, driver):
         auth = web_pages.AuthPage(driver=driver)
-
         auth.login(user=test_data.standard_user)
-        time.sleep(2)
+        # после логина логично проверить данные в профиле пользователя
+        # на сайте такой возможности нет
+        # проверим просто текст лого (текст лого в xpath)
+        res_auth = auth.check_visibility_logo()
+        assert res_auth is True, "Login failed. Logo is not visible."
