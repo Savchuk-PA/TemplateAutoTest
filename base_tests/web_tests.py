98da8541a42634e3ed5218d077ab4073fa2e2d65
Pavel <savchukpa@icloud.com>
2025-08-07 19:16:04 +0300

    add: pytest.ini change TestUser -> TestAuthorization marker smoke test

--- base_tests/web_tests.py
+++ base_tests/web_tests.py
@@ -1,14 +1,15 @@
 import time
 
 import allure
-
+import pytest
 import web as web_pages
 import test_data
 
 
-class TestUsers:
+class TestAuthorization:
 
-    @allure.title("Test Users")
+    @allure.title("Test Authorization")
+    @pytest.mark.smoke
     def test_login(self, driver):
         auth = web_pages.AuthPage(driver=driver)
         auth.login(user=test_data.standard_user)
