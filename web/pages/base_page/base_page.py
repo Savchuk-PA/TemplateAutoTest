1087ce4b9c06e56e1a8b16497a8c887596522cee
Pavel <savchukpa@icloud.com>
2025-08-07 16:14:11 +0300

    add: BasePage

---
+++ web/pages/base_page/base_page.py
@@ -0,0 +1,57 @@
+import allure
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from settings import settings
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.host = settings.web.host
+        self.driver = driver
+        print(self.host)
+
+    @allure.step("Open url a browser")
+    def open(self, url):
+        self.driver.get(url)
+
+    @allure.step("Open url in to object page")
+    def open_self(self):
+        self.driver.get(f"{self.host}{self.endpoint}")
+
+    @allure.step("Find a visible element")
+    def element_is_visible(self, locator, timeout=20):
+        self.go_to_element(self.element_is_present(locator))
+        return wait(self.driver, timeout).until(
+            EC.visibility_of_element_located(locator)
+        )
+
+    @allure.step("Find visible elements")
+    def elements_are_visible(self, locator, timeout=20):
+        return wait(self.driver, timeout).until(
+            EC.visibility_of_all_elements_located(locator)
+        )
+
+    @allure.step("Find a present element")
+    def element_is_present(self, locator, timeout=20):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    @allure.step("Find present elements")
+    def elements_are_present(self, locator, timeout=20):
+        return wait(self.driver, timeout).until(
+            EC.presence_of_all_elements_located(locator)
+        )
+
+    @allure.step("Find a not visible element")
+    def element_is_not_visible(self, locator, timeout=20):
+        return wait(self.driver, timeout).until(
+            EC.invisibility_of_element_located(locator)
+        )
+
+    @allure.step("Find clickable elements")
+    def element_is_clickable(self, locator, timeout=20):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    @allure.step("Go to specified element")
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
